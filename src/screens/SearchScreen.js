351b46a7e4e48989595c3ca5df54ebe1e2328b7b
Shrinivas <shrinivas@Shrinivass-MacBook-Pro.local>
2020-10-28 09:04:30 +0530

    second commit finished the list almost

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 function SearchScreen() {
   const [term, setTerm] = useState("");
   const [searchApi, results, errMsg] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -16,6 +23,9 @@ function SearchScreen() {
 
       <Text>{errMsg ? <Text>{errMsg}</Text> : null}</Text>
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice("$")} title="cost effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
     </View>
   );
 }
